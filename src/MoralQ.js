37e05e72e9293185fd7036e038561a5279b4b260
Abdrahmaner <135958801+Abdrahmaner@users.noreply.github.com>
2023-12-22 21:54:44 +0100

    Update MoralQ.js

--- src/MoralQ.js
+++ src/MoralQ.js
@@ -1,99 +1,56 @@
-.container {
-    font-family: sans-serif;
-    margin: auto;
-    display: flex; /* Use 'display' instead of 'position' to set it as a flex container */
-    flex-direction: column; /* Add this to make the children stack vertically */
-    align-items: center; /* Center items horizontally */
-    width: 40%;
-    background-color: azure;
-    border-radius: 6px;
-    margin-top: 50px;
-    font-size: 150%;
-}
-.analysis-container {
-    max-width: 800px;
-    margin: 50px auto;
-}
-
-.analysis {
-    padding: 20px;
-    margin-bottom: 20px;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-.yes {
-    background-color: #c1e5a8;
-    color: black;
-font-size:30px;
-}
-
-.no {
-    background-color: #f7b7b7;
-    color: black;
-font-size:30px;
-}
-
-.analysis p {
-    margin: 0;
-    font-size: 16px;
-    line-height: 1.5;
-}
-.space{ margin:15px;padding:15px;}
-.btn {
-position: relative;
-margin-top:5px;
-  display: block;
-  width: 200px;
-  height: 36px;
-  border-radius: 18px;
-  background-color: #1c89ff;
-  border: solid 1px transparent;
-  color: #fff;
-  font-size: 18px;
-  font-weight: 300;
-  cursor: pointer;
-  transition: all .1s ease-in-out;
-  &:hover {
-    background-color: lightblue;
-    border-color: #fff;
-    transition: all .1s ease-in-out;
+import React from "react";
+import "./style.css";
+import {useState} from "react";
+export default function MoralQ(){
+const [liste,SetListe]=useState([
+  "واش كتعتقد أن الكذب يمكن أن يكون مبررًا في حالات معينة؟",
+  "واش كتفضل تقديم الدعم المالي للفقراء على حساب تحسين الظروف الاقتصادية لنفسك؟",
+  "واش كتعتقد أن الانتقام يمكن أن يكون شكلًا من أشكال العدالة؟",
+  "واش كتعتبر الحب خطأً إذا كان بين أشخاص من أديان أو ثقافات مختلفة؟",
+  "واش كتفضل العمل من أجل تحقيق الرغبات الشخصية على حساب مصلحة المجتمع؟",
+  "واش كتعتقد أنه يمكن تبرير التمييز على أساس الجنس أو العرق في بعض الحالات؟",
+  "واش كتعتقد أن الحكومة يجب أن تتدخل في حياة الأفراد للحفاظ على النظام؟",
+  "واش كتعتبر أن التحالفات السياسية يمكن أن تكون أخلاقية حتى إذا كانت تتناقض مع قيمك الشخصية؟",
+  "واش كتعتقد أن السلام يمكن تحقيقه بغض النظر عن الوسائل المستخدمة؟",
+  "واش كتعتبر تقديم التضحية الشخصية من أجل الآخرين واجبًا أخلاقيًا؟"
+]
+)
+const [choice,SetChoice]=useState([])
+const [yes,SetYes]=useState(0)
+const [no,SetNo]=useState(0)
+const [index,SetIndex]=useState(0)
+const [checked,SetChecked]=useState(true)
+  const submition = () =>{
+	  SetIndex(index+1)
+	  SetChecked(true)
+	  SetChoice((choice)=>[...choice,checked? "yes":"no"])
+	  if(choice[choice.length-1]=="yes"){SetYes(yes+1)}else{SetNo(no+1)}
   }
-  
-}
-
-
-h1 {
-    text-align: center; /* Center align the text */
-    margin-bottom: 20px; /* Add margin at the bottom for spacing */
-}
-
-.flex {
-    display: flex; /* Use 'display' instead of 'position' to set it as a flex container */
-    align-items: center; /* Center items horizontally */
-    margin: 10px;
-}
-
-.imageStyle  {
-    width: 200px;
-    height: 255px; 
-    object-fit: cover; /* Use 'object-fit' instead of 'objectFit' */
-    margin: 20px;
-}
-
-/* Additional styles for the "Or" text */
-.flex > b {
-    margin: 0 20px; /* Add margin for spacing on both sides of the "Or" text */
-    font-size: 24px; /* You can adjust the font size as needed */
-}
-.imageStylee  {
-    width: 300px;
-    height: 355px; 
-    object-fit: cover; /* Use 'object-fit' instead of 'objectFit' */
-    margin: 20px;	
-}
-.won{
-	color : green;
-	font-size:40px;
-	font-family:serif;
+	return(
+	<>
+	<div className="container">
+	{choice.length==10? <><div>
+المهم النسبة المئوية ديالك من اه و لا هي {yes*100/10}%  اه  و %{no*100/10} من لا</div>
+	
+	<div className="analysis-container">
+       {yes*100/10 > no*100/10?<><div className="analysis yes">
+        <p> كتبان عليك واحد الشخص متفتح ومفتوح على التفاوض والتحول فوجهات نظرك. كتظهر أن عندك استعداد لتبرير بعض الأمور بمواقف محددة، وكتكون عندك القدرة على استغلال الفرص لمصلحتك الشخصية وكتهتم بالناس من حولك. يمكن قول أنك واحد من الناس اللي كيتواجدوا بشكل إيجابي فالحياة، وكتفكر بالتفاصيل والسياق قبل ماتخدم أي قرار.</p>
+          </div></>:
+		  <div className="analysis no">
+      <p>واش بزاف ديال جواباتك كانو لا؟ بصح كنقدروك على الالتزام ديالك بقيمك ومبادئك. كتبان عليك واحد الشخص متمسك بالقيم الشخصية بشكل قوي وكتفضل ثابت فآرائك. ممكن نقول أنك واحد من اللي معندوش مركز معين لتنحي، وكتمشي مع مبادئك حتى فواجهة التحديات. يمكن قول أنك متضامن مع المبادئ الأخلاقية ومستعد تتضحى بالمصلحة الشخصية باش تحترمها.</p>
+    </div>
+	   }
+   </div>
+	</> :
+		
+	 <><h1>أسئلة أخلاقية  </h1>
+	<div>{liste[index]}</div>
+	<div className="space">
+	<input type="radio" value="yes" checked = {checked} onChange={(e) => SetChecked(true)} name ="m"/>اه  <br/>
+	<input type="radio" value="no" checked = {!checked}  onChange={(e) => SetChecked(false)}  name ="m"/>لا <br/>
+	<input className = "btn" type="button" onClick={submition} value="التالي " />
+	</div></>}
+	</div>
+	</>
+	)
 }
